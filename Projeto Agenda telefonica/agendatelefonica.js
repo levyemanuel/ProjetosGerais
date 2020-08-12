81add322d22123bac1e3fe2bb67713fb32b55db3
gabrielsiebra0203 <62256561+gabrielsiebra0203@users.noreply.github.com>
2020-08-12 09:38:24 -0300

    Add files via upload

---
+++ Projeto Agenda telefonica/agendatelefonica.js
@@ -0,0 +1,8 @@
+$('.tic').click(function(){
+    $('.menulateral ul .itenstic').toggleClass('mostra')
+    console.log('teste')
+    });
+    $('.rh').click(function(){
+        $('.menulateral ul .itensRH').toggleClass('mostra')
+        console.log('teste')
+        });
