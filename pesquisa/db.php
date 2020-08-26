a3baef0d291fe73ba1b5ef7be86aa3850710a33f
caio205info <62343735+caio205info@users.noreply.github.com>
2020-08-26 10:35:06 -0300

    Delete db.php

--- pesquisa/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-    header('Content-Type: text/html; charset=utf-8');
-    
-    $con = mysqli_connect('localhost', 'root', '123456', 'agenda');
-
-    if (!$con){
-        die('Erro na conexao'.mysqli_error());
-    }
